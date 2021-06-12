a4aa4cb41bf28e994ad0c930bd313131790c5153
MIR SHAHIDUZZAMAN <mshahiduzzaman@fordham.edu>
2021-06-12 15:59:41 -0400

    fire_slider + progress_slider_ name fixed

--- GameDownload/Assets/Scripts/progress_slider_.cs
+++ GameDownload/Assets/Scripts/progress_slider_.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class progress_slider : MonoBehaviour
+public class progress_slider_ : MonoBehaviour
 {
     private Slider slider;
     private float targetProgress = 0;
     public float fillspeed = 0.1f;
 
-    private void Awake() {
+    private void Awake() 
+    {
         slider = gameObject.GetComponent<Slider>();
     }
     // Start is called before the first frame update
@@ -21,13 +22,15 @@ public class progress_slider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(slider.value < targetProgress){
+        if(slider.value < targetProgress)
+        {
             slider.value += fillspeed*Time.deltaTime/10;//devide by number (10 for now) to slow the progress bar
         }
     }
 
     //Add progress to the bar
-    public void incrementProgress(float newProgress){
+    public void incrementProgress(float newProgress)
+    {
         targetProgress = slider.value + newProgress;
     }
 }
