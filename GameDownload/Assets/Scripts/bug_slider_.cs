bd5207b5079a5158652d747ef55cdcc0c2a8fa7a
MIR SHAHIDUZZAMAN <mshahiduzzaman@fordham.edu>
2021-06-12 16:03:14 -0400

    Fixed Script File Names

--- GameDownload/Assets/Scripts/bug_slider_.cs
+++ GameDownload/Assets/Scripts/bug_slider_.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class fire_slider : MonoBehaviour
+public class bug_slider_ : MonoBehaviour
 {
     private Slider slider;
     private float targetProgress = 0;
     public float fillspeed = 0.1f;
 
-    private void Awake() 
-    {
+    private void Awake() {
         slider = gameObject.GetComponent<Slider>();
     }
     // Start is called before the first frame update
@@ -22,15 +21,13 @@ public class fire_slider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(slider.value < targetProgress)
-        {
+        if(slider.value < targetProgress){
             slider.value += fillspeed*Time.deltaTime/10;//devide by number (10 for now) to slow the progress bar
         }
     }
 
     //Add progress to the bar
-    public void incrementProgress(float newProgress)
-    {
+    public void incrementProgress(float newProgress){
         targetProgress = slider.value + newProgress;
     }
 }
